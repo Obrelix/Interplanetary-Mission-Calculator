cf2e9ea6ad49403c33c89137ed92a05d096ef9a3
Obrelix <Obrelix@OBRELIXPC>
2018-05-26 13:32:29 +0300

    Initial Commit

---
+++ Mission Calculator/Mission Calculator/Pages/pgMissionCalculator.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mission_Calculator.Pages
+{
+    /// <summary>
+    /// Interaction logic for pgMissionCalculator.xaml
+    /// </summary>
+    public partial class pgMissionCalculator : Page
+    {
+        public pgMissionCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void textBlockStop_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radioButtonNormal_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
