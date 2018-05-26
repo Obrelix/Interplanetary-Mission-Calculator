10a9c69b97a1eab8f1e9538cea3cd7d0624b3aae
Obrelix <Obrelix@OBRELIXPC>
2018-05-27 00:23:27 +0300

    Basic Classes Added

---
+++ Mission Calculator/Mission Calculator/Classes/SelestialObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mission_Calculator.Enumerators;
+
+namespace Mission_Calculator.Classes
+{
+    public abstract class SelestialObject
+    {
+        #region "General Properties"
+        protected string Name;
+        protected int Index;
+        protected int ParentIndex;
+        protected string ImageUri;
+        protected Types Type;
+        protected Systems System;
+        protected Orbits Orbits;
+        #endregion
+        #region "Technical Properties"
+        protected double OrbitalPeriod;
+        protected double SphereOfInfluence;
+        protected double SurfaceGravity;
+        protected bool HasAtmosphere;
+        protected bool HasOxigen;
+        protected double AtmosphericPressure;
+        protected double LowerAtmoHeight;
+        protected double UpperAtmoHeight;
+        protected double LowOrbitHeight;
+        protected double EscapeVelocity;
+        #endregion
+        #region "Science Relative Properties"
+        protected int TotalBiomesCount;
+        protected double SMSurface;
+        protected double SMLowerAtmosphere;
+        protected double SMUpperAtmosphere;
+        protected double SMNearSpace;
+        protected double SMOuterSpace;
+        #endregion
+        #region "Delta V Relative Properties"
+        protected int SurfaceToLowOrbit;
+        protected int LowOrbitToMoonIntercept;
+        protected int MoonInterceptToElipticalOrbit;
+        protected int MoonInterceptToElipticalOrbitMPC;
+        protected int LowOrbitToElipticalOrbit;
+        protected int ElipticalOrbitToPlanetIntercet;
+        protected int PlanetInterceptToStarElipticalOrbit;
+        protected int MaxPlaneChange;
+        #endregion
+    }
+}
---
+++ Mission Calculator/Mission Calculator/Enumerators/Enumerators.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_Calculator.Enumerators
+{
+    public class GameConstants
+    {
+        public const int StockPlanetsCount = 18;
+        public const int OuterPlanetsCount = 32;
+        public const int RssPlanetsCount = 17;
+        public const int KerbinTime = 21600; //seconds per day
+        public const int EarthTime = 86400; //seconds per day
+
+    }
+
+    public enum Types : Int16 
+    {
+        Star = 0,
+        Planet = 1,
+        Moon = 2,
+        Gasgiant = 3,
+        UFO = 4
+    }
+
+    public enum Systems
+    {
+        Kerbol = 0,
+        Moho = 1,
+        Eve = 2,
+        Kerbin = 3,
+        Duna = 4,
+        Dres = 5,
+        Jool = 6,
+        Eeloo = 7,
+        Sarnus = 8,
+        Urlum = 8,
+        Neidon = 9,
+        Plock = 10,
+        Earth = 11,
+        Mercury = 12,
+        Venus = 13,
+        Mars = 14,
+        Jupiter = 15,
+        Saturn = 16,
+        Uranus = 17,
+        Neptune = 18,
+        None = 19
+    }
+
+    public enum Orbits
+    {
+        Sun = 0,
+        Kerbol = 1,
+        Eve = 2,
+        Kerbin = 3,
+        Duna = 4,
+        Jool = 5,
+        Sarnus = 6,
+        Urlum = 7,
+        Neidon = 8,
+        Plock = 9,
+        Earth = 10,
+        Mercury = 11,
+        Venus = 12,
+        Mars = 13,
+        Jupiter = 14,
+        Saturn = 15,
+        Uranus = 16,
+        Neptune = 17,
+        None = 18
+    }
+    
+}
