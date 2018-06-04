dfc33511abd1c3ee4c710b21d6ba539f9b82ef5b
Obrelix <kobladoures@gmail.com>
2018-06-04 22:48:10 +0300

    Mass Commit

--- Mission Calculator/Mission Calculator/Classes/SelestialObject.cs
+++ Mission Calculator/Mission Calculator/Classes/SelestialObject.cs
@@ -13,6 +13,37 @@ namespace Mission_Calculator.Classes
 
         #region "Private Properties"
 
+        private static string SemiMajorAxisUM= "m";
+        private static string ApoapsisUM= "m";
+        private static string PeriapsisUM= "m";
+        private static string OrbitalInclinationUM= "°";
+        private static string ArgumentOfPeriapsisUM= "°";
+        private static string LongitudeOfTheAscendingNodeUM= "°";
+        private static string MeanAndomalyUM= "rad";
+        private static string OrbitalPeriodUM = "s";
+        private static string OrbitalVelocityUM= "m/s";
+
+        private static string EquatorialRadiusUM= "m";
+        private static string EquatorialCircumferenceUM= "m";
+        private static string SurfaceAreaUM= "m²";
+        private static string MassUM= "kg";
+        private static string StandarGravitonialParameterUM= "m²/s²";
+        private static string DensityUM= "kg/m³";
+        private static string SurfaceGravityUM= "m/s²";
+        private static string EscapeVelocityUM= "m/s";
+        private static string SiderealRotationPeriodUM= "s";
+        private static string SolarDayUM= "s";
+        private static string SiderealRotationVelocityUM= "m/s";
+        private static string SynchronousOrbitUM= "km";
+        private static string SphereOfInfluenceUM= "m";
+
+        private static string AtmosphericPressureUM= "atm";
+        private static string ScaleHeightUM= "m";
+        private static string AtmosphericHeightUM= "m";
+        private static string LowOrbitHeightUM= "km";
+        private static string TemperatureUM= "°C";
+        private static string DeltaVUM= "m/s";
+
         #endregion
 
         #region "Public Properties"
@@ -21,23 +52,51 @@ namespace Mission_Calculator.Classes
         public string Name { get; set; }
         public int Index { get; set; }
         public string ImageUri { get; set; }
+        public string BiomeImageUri { get; set; }
         public Types Type { get; set; }
         public Systems System { get; set; }
         public Orbits Orbits { get; set; }
         public int ParentObjectIndex { get; set; }
         #endregion
 
-        #region "Technical Properties"
+        #region "Orbital Characteristics"
+        public double SemiMajorAxis { get; set; }
+        public double Apoapsis { get; set; }
+        public double Periapsis { get; set; }
+        public double OrbitalEccentricity { get; set; }
+        public double OrbitalInclination { get; set; }
+        public double ArgumentOfPeriapsis { get; set; }
+        public double LongitudeOfTheAscendingNode { get; set; }
+        public double MeanAndomaly { get; set; }
+        public double OrbitalVelocity { get; set; }
         public double OrbitalPeriod { get; set; }
-        public double SphereOfInfluence { get; set; }
+        #endregion
+
+        #region "Phisical Characteristics"
+        public double EquatorialRadius { get; set; }
+        public double EquatorialCircumference { get; set; }
+        public double SurfaceArea { get; set; }
+        public double Mass { get; set; }
+        public double StandarGravitonialParameter { get; set; }
+        public double Density { get; set; }
         public double SurfaceGravity { get; set; }
-        public bool HasAtmosphere { get; set; }
-        public bool HasOxigen { get; set; }
-        public double AtmosphericPressure { get; set; }
-        public double LowerAtmoHeight { get; set; }
-        public double UpperAtmoHeight { get; set; }
-        public double LowOrbitHeight { get; set; }
         public double EscapeVelocity { get; set; }
+        public double SiderealRotationPeriod { get; set; }
+        public double SolarDay { get; set; }
+        public double SiderealRotationVelocity { get; set; }
+        public double SynchronousOrbit { get; set; }
+        public double SphereOfInfluence { get; set; }
+        #endregion
+
+        #region "Atmospheric Characteristics"
+        public bool AtmospherePresent { get; set; }
+        public bool OxygenPresent { get; set; }
+        public double AtmosphericPressure { get; set; }
+        public double ScaleHeight { get; set; }
+        public double AtmosphericHeight { get; set; }
+        public double LowOrbitHeight { get; set; }
+        public double TemperatureMin { get; set; }
+        public double TemperatureMax { get; set; }
         #endregion
 
         #region "Science Relative Properties"
@@ -70,24 +129,49 @@ namespace Mission_Calculator.Classes
         /// Constractor
         /// </summary>
         /// <param name="parentObject"></param>
-        public SelestialObject(SelestialObject parentObject = null)
+        public SelestialObject()
         {
             this.Name = "None";
             this.Index = 0;
             this.ImageUri = @"pack://application:,,/Images/Planets/None.png";
+            this.BiomeImageUri = @"pack://application:,,/Images/Planets/None.png";
             this.Type = Types.Star;
             this.System = Systems.None;
-            this.OrbitalPeriod = 0;
-            this.SphereOfInfluence = 0;
-            this.LowOrbitHeight = 0;
-            this.SurfaceGravity = 0;
-            this.HasAtmosphere = false;
-            this.HasOxigen = false;
-            this.AtmosphericPressure = 0;
-            this.LowerAtmoHeight = 0;
-            this.UpperAtmoHeight = 0;
-            this.LowOrbitHeight = 0;
-            this.EscapeVelocity = 0;
+
+            this.SemiMajorAxis= 0;
+            this.Apoapsis= 0;
+            this.Periapsis= 0;
+            this.OrbitalEccentricity= 0;
+            this.OrbitalInclination= 0;
+            this.ArgumentOfPeriapsis= 0;
+            this.LongitudeOfTheAscendingNode= 0;
+            this.MeanAndomaly= 0;
+            this.OrbitalPeriod= 0;
+            this.OrbitalVelocity = 0;
+
+            this.EquatorialRadius= 0;
+            this.EquatorialCircumference= 0;
+            this.SurfaceArea= 0;
+            this.Mass= 0;
+            this.StandarGravitonialParameter= 0;
+            this.Density= 0;
+            this.SurfaceGravity= 0;
+            this.EscapeVelocity= 0;
+            this.SiderealRotationPeriod= 0;
+            this.SolarDay= 0;
+            this.SiderealRotationVelocity= 0;
+            this.SynchronousOrbit= 0;
+            this.SphereOfInfluence= 0;
+
+            this.AtmospherePresent= false;
+            this.OxygenPresent= false;
+            this.AtmosphericPressure= 0;
+            this.ScaleHeight= 0;
+            this.AtmosphericHeight= 0;
+            this.LowOrbitHeight= 0;
+            this.TemperatureMin= 0;
+            this.TemperatureMax= 0;
+
             this.TotalBiomesCount = 0;
             this.SMSurface = 0;
             this.SMLowerAtmosphere = 0;
@@ -152,22 +236,21 @@ namespace Mission_Calculator.Classes
             if (this.Type == Types.Moon) strSpecs += "    Moon of : " + this.System;
 
             strSpecs += Environment.NewLine;
-            strSpecs += "Surface Gravity : " + this.SurfaceGravity + " m/s²    Low Orbit : " + this.LowOrbitHeight;
+            strSpecs += "Surface Gravity : " + this.SurfaceGravity + " m/s²    Low Orbit : " + this.LowOrbitHeight + "km    Low Orbit : ";
             strSpecs += Environment.NewLine;
-            strSpecs += "Escape Velocity : " + this.EscapeVelocity + " m/s    Sphere of influence : " + this.SphereOfInfluence;
+            strSpecs += "Escape Velocity : " + this.EscapeVelocity.ToString("N") + " m/s    Sphere of influence : " + this.SphereOfInfluence.ToString("N") + " m";
             strSpecs += Environment.NewLine;
-            strSpecs += "Atmosphere Present: " + this.HasAtmosphere;
+            strSpecs += "Atmosphere Present: " + this.AtmospherePresent;
 
-            if (this.HasAtmosphere) strSpecs += "   Oxygen Present : " + this.HasOxigen + "    Pressure : " + this.AtmosphericPressure + " atm";
+            if (this.AtmospherePresent) strSpecs += "   Oxygen Present : " + this.OxygenPresent + "    Pressure : " + this.AtmosphericPressure + " atm";
             
             strSpecs += Environment.NewLine;
-            strSpecs += "Biomes : " + this.TotalBiomesCount + "   Scientific Multiplier:    Surface : " + this.SMSurface + "    Lower atmo : " + this.SMLowerAtmosphere;
+            strSpecs += "Biomes : " + this.TotalBiomesCount + "   Scientific Multiplier:[  S : " + this.SMSurface + "    LA : " + this.SMLowerAtmosphere;
+            strSpecs += "    UA : " + this.SMUpperAtmosphere + "   NS : " + this.SMNearSpace + "   OS : " + this.SMOuterSpace + "  ]";
             strSpecs += Environment.NewLine;
-            strSpecs += "Upper Atmo : " + this.SMUpperAtmosphere + "   Near Space : " + this.SMNearSpace + "   Outer Space : " + this.SMOuterSpace;
-            strSpecs += Environment.NewLine;
-            strSpecs += " Δv info : " + "   Surface to Low Orbit : " + string.Format("{0:n0}", this.SurfaceToLowOrbit) + "m/s    Low Orbit to SOI Edge : " +
+            strSpecs += "Δv info : " + "   Surface to Low Orbit : " + string.Format("{0:n0}", this.SurfaceToLowOrbit) + " m/s    Low Orbit to SOI Edge : " +
                             string.Format("{0:n0}", (this.LowOrbitToMoonIntercept + this.MoonInterceptToElipticalOrbit + this.ElipticalOrbitToPlanetIntercet + this.LowOrbitToElipticalOrbit +
-                                                     this.PlanetInterceptToStarElipticalOrbit)) + "m/s";
+                                                     this.PlanetInterceptToStarElipticalOrbit)) + " m/s";
             return strSpecs;
         }
 
--- Mission Calculator/Mission Calculator/Pages/pgMissionCalculator.xaml.cs
+++ Mission Calculator/Mission Calculator/Pages/pgMissionCalculator.xaml.cs
@@ -24,10 +24,7 @@ namespace Mission_Calculator.Pages
     public partial class pgMissionCalculator : Page
     {
         #region "General Declaration"
-
-        List<SelestialObject> StockPlanetList;
-        List<SelestialObject> OuterPlanetList;
-        List<SelestialObject> RSSPlanetList;
+        
         List<SelestialObject> currentPlanetList;
         static string saveDirectoryPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Interplanetary Mission Calulator";
         static string planeDataDirectoryPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\PlanetsData";
@@ -41,8 +38,9 @@ namespace Mission_Calculator.Pages
         {
             InitializeComponent();
             errorFileInit();
-            planetFilesInit();
-            comboboxesInit();
+            cboGameMode.SelectedIndex = 0;
+            cboTimeUnit.SelectedIndex = 1;
+            planetFilesInit(0);
         }
 
         #region "Methods"
@@ -54,16 +52,34 @@ namespace Mission_Calculator.Pages
             IO.createFile(errorFilePath, "");
         }
 
-        private void planetFilesInit()
+        private void planetFilesInit(int mode)
         {
-            StockPlanetList = IO.LoadListFromFile(StockPlanetsPath);
-            OuterPlanetList = IO.LoadListFromFile(OuterPlanetsPath);
-            RSSPlanetList = IO.LoadListFromFile(RSSPlanetsPath);
+            if(currentPlanetList != null)currentPlanetList.Clear();
+            switch (mode)
+            {
+                case 0:
+                    currentPlanetList = IO.LoadListFromFile(StockPlanetsPath);
+                    break;
+                case 1:
+                    currentPlanetList = IO.LoadListFromFile(OuterPlanetsPath);
+                    break;
+                case 2:
+                    currentPlanetList = IO.LoadListFromFile(RSSPlanetsPath);
+                    break;
+                default:
+                    currentPlanetList = IO.LoadListFromFile(StockPlanetsPath);
+                    break;
+            }
+            comboboxesInit();
         }
 
         private void comboboxesInit()
         {
-            foreach (SelestialObject obj in StockPlanetList)
+            if (comboBoxOrigin.Items != null) comboBoxOrigin.Items.Clear();
+            if (comboBoxStop1.Items != null) comboBoxStop1.Items.Clear();
+            if (comboBoxStop2.Items != null) comboBoxStop2.Items.Clear();
+            if (comboBoxStop3.Items != null) comboBoxStop3.Items.Clear();
+            foreach (SelestialObject obj in currentPlanetList)
             {
                 comboBoxOrigin.Items.Add(obj.Name);
                 comboBoxStop1.Items.Add(obj.Name);
@@ -83,24 +99,36 @@ namespace Mission_Calculator.Pages
                 switch (cboName)
                 {
                     case "comboBoxOrigin":
-                        if (comboBoxOrigin.SelectedIndex != 0) textBlockOrigin.Text = StockPlanetList[comboBoxOrigin.SelectedIndex].ToString();
-                        else textBlockOrigin.Text = "Origin";
-                        imageOrigin.Source = new BitmapImage(new Uri(StockPlanetList[comboBoxOrigin.SelectedIndex].ImageUri));
+                        if (comboBoxOrigin.SelectedIndex != -1)
+                        {
+                            if (comboBoxOrigin.SelectedIndex != 0) textBlockOrigin.Text = currentPlanetList[comboBoxOrigin.SelectedIndex].ToString();
+                            else textBlockOrigin.Text = "Origin";
+                            imageOrigin.Source = new BitmapImage(new Uri(currentPlanetList[comboBoxOrigin.SelectedIndex].ImageUri));
+                        }
                         break;
                     case "comboBoxStop1":
-                        if (comboBoxStop1.SelectedIndex != 0) textBlockStop1.Text = StockPlanetList[comboBoxStop1.SelectedIndex].ToString();
-                        else textBlockStop1.Text = "Stop 1";
-                        imageStop1.Source = new BitmapImage(new Uri(StockPlanetList[comboBoxStop1.SelectedIndex].ImageUri));
+                        if (comboBoxStop1.SelectedIndex != -1)
+                        {
+                            if (comboBoxStop1.SelectedIndex != 0) textBlockStop1.Text = currentPlanetList[comboBoxStop1.SelectedIndex].ToString();
+                            else textBlockStop1.Text = "Stop 1";
+                            imageStop1.Source = new BitmapImage(new Uri(currentPlanetList[comboBoxStop1.SelectedIndex].ImageUri));
+                        }
                         break;
                     case "comboBoxStop2":
-                        if (comboBoxStop2.SelectedIndex != 0) textBlockStop2.Text = StockPlanetList[comboBoxStop2.SelectedIndex].ToString();
-                        else textBlockStop2.Text = "Stop 2";
-                        imageStop2.Source = new BitmapImage(new Uri(StockPlanetList[comboBoxStop2.SelectedIndex].ImageUri));
+                        if (comboBoxStop2.SelectedIndex != -1)
+                        {
+                            if (comboBoxStop2.SelectedIndex != 0) textBlockStop2.Text = currentPlanetList[comboBoxStop2.SelectedIndex].ToString();
+                            else textBlockStop2.Text = "Stop 2";
+                            imageStop2.Source = new BitmapImage(new Uri(currentPlanetList[comboBoxStop2.SelectedIndex].ImageUri));
+                        }
                         break;
                     case "comboBoxStop3":
-                        if (comboBoxStop3.SelectedIndex != 0) textBlockStop3.Text = StockPlanetList[comboBoxStop3.SelectedIndex].ToString();
-                        else textBlockStop3.Text = "Stop 3";
-                        imageStop3.Source = new BitmapImage(new Uri(StockPlanetList[comboBoxStop3.SelectedIndex].ImageUri));
+                        if (comboBoxStop3.SelectedIndex != -1)
+                        {
+                            if (comboBoxStop3.SelectedIndex != 0) textBlockStop3.Text = currentPlanetList[comboBoxStop3.SelectedIndex].ToString();
+                            else textBlockStop3.Text = "Stop 3";
+                            imageStop3.Source = new BitmapImage(new Uri(currentPlanetList[comboBoxStop3.SelectedIndex].ImageUri));
+                        }
                         break;
                     default:
                         break;
@@ -133,16 +161,7 @@ namespace Mission_Calculator.Pages
                     break;
             }
         }
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void radioButtonNormal_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
+        
 
         private void Main_Loaded(object sender, RoutedEventArgs e)
         {
@@ -160,5 +179,15 @@ namespace Mission_Calculator.Pages
         }
 
         #endregion
+
+        private void cboGameMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            planetFilesInit(((ComboBox)sender).SelectedIndex);
+        }
+
+        private void MainEvent(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
