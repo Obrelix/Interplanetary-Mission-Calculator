77b00c42dd84cb577669e011357a2a9ef4e1a15d
Obrelix <kobladoures@gmail.com>
2018-06-11 02:49:21 +0300

    Date format fixed

--- Mission Calculator/Mission Calculator/Classes/Globals.cs
+++ Mission Calculator/Mission Calculator/Classes/Globals.cs
@@ -42,7 +42,7 @@ namespace Mission_Calculator.Classes
             try
             {
                 TimeSpan t = TimeSpan.FromSeconds(secs);
-                return string.Format("{0:D2} h {1:D2} m {2:D2} s", t.Hours, t.Minutes, t.Seconds);
+                return string.Format("{0:D2} d {1:D2} h {2:D2} m", t.Days, t.Hours, t.Minutes);
             }
             catch (Exception)
             {
